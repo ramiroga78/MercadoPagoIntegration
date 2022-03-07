ff6e50a4dad69a47723b2c948a54c6f4492c01b5
ramiroga78 <98047761+ramiroga78@users.noreply.github.com>
2022-03-07 19:09:16 -0300

    Add files via upload

---
+++ MercadoPagoControlador.cs
@@ -0,0 +1,212 @@
+﻿using log4net;
+using MercadoPago.Client.Common;
+using MercadoPago.Client.Preference;
+using MercadoPago.Config;
+using MercadoPago.Resource.Preference;
+using Negocio.BIS;
+using Negocio.DATA;
+using Negocio.MODELOS_LINQ;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Web.Configuration;
+
+namespace Aplicacion.Controlador
+{
+    public class MercadoPagoControlador
+    {
+        public static DataTable GetPaymentURL(string conexion, int idUsuario, Pedido pedido)
+        {
+            try
+            {
+                Usuario usuario = UsuarioControlador.getUsuarioById(conexion, idUsuario);
+
+                UbicacionUsuario ubicacionUsuario = UbicacionUsuarioControlador.GetUbicacionUsuarioById(conexion, pedido.id_ubicacion_usuario);
+
+                List<DetallePedido> detallePedidoList = DetallePedidoControlador.DetallePedidoIdPedido(conexion, pedido.id);
+
+                string backendURL = WebConfigurationManager.AppSettings["BackendURL"].ToString();
+                MercadoPagoConfig.AccessToken = WebConfigurationManager.AppSettings["MercadoPagoAccessToken"].ToString();
+
+                // Crea el request con múltiples ítems
+                var preferenceRequest = new PreferenceRequest
+                {
+                    ExternalReference = pedido.id.ToString(),
+                    Items = new List<PreferenceItemRequest>(),
+                    Shipments = new PreferenceShipmentsRequest
+                    {
+                        Cost = pedido.precioEnvio, //Costo especificado del envío
+                        Mode = "not_specified"
+                    },
+                    Payer = new PreferencePayerRequest
+                    {
+                        Name = usuario.nombre,
+                        Surname = "",
+                        Email = usuario.mail,
+                        Phone = new PhoneRequest
+                        {
+                            AreaCode = "",
+                            Number = usuario.telefono
+                        },
+                        Identification = new IdentificationRequest
+                        {
+                            Type = "DNI",
+                            Number = usuario.DNI
+                        },
+                        Address = new AddressRequest
+                        {
+                            ZipCode = "",
+                            StreetName = ubicacionUsuario.ubicacion,
+                            StreetNumber = ""
+                        }
+                    },
+                    PaymentMethods = new PreferencePaymentMethodsRequest
+                    {
+                        ExcludedPaymentTypes = new List<PreferencePaymentTypeRequest>
+                    {
+                        new PreferencePaymentTypeRequest
+                        {
+                            Id = "ticket",
+                        },
+                    },
+                        Installments = 1,
+                    },
+                    StatementDescriptor = "Company_Name",
+                    BinaryMode = true,
+                    Expires = true,
+                    ExpirationDateTo = DateTime.Now.AddDays(1),
+                    NotificationUrl = backendURL + "/api/PaymentNotification"
+                };
+
+                foreach (DetallePedido item in detallePedidoList)
+                {
+                    PreferenceItemRequest preferenceItemRequest = new PreferenceItemRequest
+                    {
+                        Id = item.idMenu.ToString(),
+                        Title = item.descripcion,
+                        Description = item.detalle,
+                        PictureUrl = backendURL + "/getimagen.aspx?id=" + item.idMenu + "&entidad=MF",
+                        CategoryId = "others", //De acuerdo a la documentación de MP: https://api.mercadopago.com/item_categories
+                        Quantity = item.cantidad,
+                        CurrencyId = "ARS",
+                        UnitPrice = item.precioFinal / item.cantidad,
+                    };
+
+                    preferenceRequest.Items.Add(preferenceItemRequest);
+                }
+
+                var client = new PreferenceClient();
+
+                Preference preference = client.Create(preferenceRequest);
+
+                MercadoPagoIntegration integrationData = new MercadoPagoIntegration
+                {
+                    IdPedido = pedido.id,
+                    Request = JsonConvert.SerializeObject(preferenceRequest),
+                    Response = JsonConvert.SerializeObject(preference),
+                    ExternalMercadoPagoId = preference.Id,
+                    MercadoPagoExternalType = "Preference",
+                    TimeStamp = DateTime.Now
+                };
+
+                Add(conexion, integrationData);
+
+                return ConvertPreferenceToDataTable(preference);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static void RefundPayment(string conexion, string authorization, int idPedido)
+        {
+            try
+            {
+                ILog _logger = LogManager.GetLogger(System.Environment.MachineName);
+
+                string paymentId = MercadoPagoIntegrationDATA.GetPaymentIdByIdPedido(conexion, idPedido);
+
+                string url = "https://api.mercadopago.com/v1/payments/" + paymentId + "/refunds";
+
+                _logger.Info("RefundPayment: URL: " + url);
+
+                HttpWebResponse httpResponse = GetResponseFromUrl(authorization, url, "POST");
+
+                if (httpResponse.StatusCode == HttpStatusCode.OK || httpResponse.StatusCode == HttpStatusCode.Created)
+                {
+                    _logger.Info("RefundPayment: Response Status: " + httpResponse.StatusCode);
+
+                    StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream());
+
+                    string response = streamReader.ReadToEnd();
+
+                    _logger.Info("RefundPayment: Response from MP API: " + response);
+
+                    MercadoPagoIntegration integrationData = new MercadoPagoIntegration
+                    {
+                        IdPedido = idPedido,
+                        Request = url,
+                        Response = JsonConvert.SerializeObject(response),
+                        ExternalMercadoPagoId = paymentId,
+                        MercadoPagoExternalType = "Refund",
+                        TimeStamp = DateTime.Now
+                    };
+
+                    Add(conexion, integrationData);
+                }
+                else
+                {
+                    _logger.Error("RefundPayment: Ocurrió un error al procesar el reembolso del pedido " + idPedido);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static HttpWebResponse GetResponseFromUrl(string authorizarion, string url, string method)
+        {
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpRequest.Method = method;
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = authorizarion;
+
+            return (HttpWebResponse)httpRequest.GetResponse();
+        }
+
+        public static void Add(string conexion, MercadoPagoIntegration integrationData)
+        {
+            MercadoPagoIntegrationDATA.Add(conexion, integrationData);
+        }
+
+        private static DataTable ConvertPreferenceToDataTable(Preference preference)
+        {
+            DataTable dataTable = new DataTable();
+
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(Preference));
+
+            foreach (PropertyDescriptor p in props)
+            {
+                dataTable.Columns.Add(p.Name, Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType);
+            }
+
+            DataRow row = dataTable.NewRow();
+
+            foreach (PropertyDescriptor prop in props)
+            {
+                row[prop.Name] = prop.GetValue(preference) ?? DBNull.Value;
+            }
+
+            dataTable.Rows.Add(row);
+
+            return dataTable;
+        }
+    }
+}
---
+++ MercadoPagoController.cs
@@ -0,0 +1,179 @@
+﻿using Aplicacion.Controlador;
+using log4net;
+using Negocio.BIS;
+using Servicios.Utiles;
+using System;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Web.Configuration;
+using Negocio.MODELOS_LINQ;
+
+namespace WebApi.Controllers
+{
+    public class MercadoPagoController : ApiController
+    {
+        private readonly ILog _logger = LogManager.GetLogger(System.Environment.MachineName);
+
+        [Route("api/PaymentNotification")]
+        [HttpPost]
+        public HttpResponseMessage PaymentNotification([FromBody] JObject jObject)
+        {
+            string conexion = WebConfigurationManager.ConnectionStrings["DelivegyDBConnectionString"].ConnectionString;
+
+            try
+            {
+                _logger.Info("PaymentNotification: INPUT: " + JsonConvert.SerializeObject(jObject));
+
+                JToken data;
+
+                if (jObject.TryGetValue("action", out data))
+                {
+                    if (data.ToString() == "payment.created")
+                    {
+                        if (jObject.TryGetValue("data", out data))
+                        {
+                            string paymentId = data["id"].ToString();
+
+                            _logger.Info("PaymentNotification: Payment id: " + paymentId);
+
+                            //Se obtienen los detalles del pago con id informado
+                            string url = "https://api.mercadopago.com/v1/payments/" + paymentId;
+
+                            _logger.Info("PaymentNotification: URL: " + url);
+
+                            string authorization = "Bearer " + WebConfigurationManager.AppSettings["MercadoPagoAccessToken"].ToString();
+
+                            HttpWebResponse httpResponse = MercadoPagoControlador.GetResponseFromUrl(authorization, url, "GET");
+
+                            if (httpResponse.StatusCode == HttpStatusCode.OK)
+                            {
+                                _logger.Info("PaymentNotification: Response Status: " + httpResponse.StatusCode);
+
+                                StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream());
+
+                                string response = streamReader.ReadToEnd();
+
+                                _logger.Info("PaymentNotification: Response from MP API: " + response);
+
+                                jObject = (JObject)JsonConvert.DeserializeObject(response);
+
+                                //Intentamos obtener el idPedido (external_reference)
+                                if (jObject.TryGetValue("external_reference", out data))
+                                {
+                                    int idPedido = Convert.ToInt32(data);
+
+                                    _logger.Info("PaymentNotification: External reference: " + idPedido);
+
+                                    MercadoPagoIntegration integrationData = new MercadoPagoIntegration
+                                    {
+                                        IdPedido = idPedido,
+                                        Request = url,
+                                        Response = JsonConvert.SerializeObject(response),
+                                        ExternalMercadoPagoId = paymentId,
+                                        MercadoPagoExternalType = "Payment",
+                                        TimeStamp = DateTime.Now
+                                    };
+
+                                    MercadoPagoControlador.Add(conexion, integrationData);
+
+                                    //Obtenemos el estado del pago
+                                    if (jObject.TryGetValue("status", out data))
+                                    {
+                                        string paymentStatus = data.ToString();
+
+                                        _logger.Info("PaymentNotification: Payment Status: " + paymentStatus);
+
+                                        //Estado del pago - de acuerdo a documentación de MP: https://www.mercadopago.com.ar/developers/es/reference/payments/_payments_id/get
+                                        //pending: El usuario no completó el proceso de pago todavía.
+                                        //approved: El pago fue aprobado y acreditado.
+                                        //authorized: El pago fue autorizado pero no capturado todavía.
+                                        //in_process: El pago está en revisión.
+                                        //in_mediation: El usuario inició una disputa.
+                                        //rejected: El pago fue rechazado. El usuario podría reintentar el pago.
+                                        //cancelled: El pago fue cancelado por una de las partes o el pago expiró.
+                                        //refunded: El pago fue devuelto al usuario.
+                                        //charged_back: Se ha realizado un contracargo en la tarjeta de crédito del comprador.
+                                        if (paymentStatus == "approved")
+                                        {
+                                            if (jObject.TryGetValue("transaction_details", out data))
+                                            {
+                                                decimal totalPaidAmount = Convert.ToDecimal(data["total_paid_amount"]);
+
+                                                _logger.Info("PaymentNotification: Total paid amount: " + totalPaidAmount);
+
+                                                //Entorno oEntorno = new Entorno();
+
+                                                //oEntorno.ConexionDatos = Aplicacion.Utilidades.ConexionBase.ConexionCuentas();
+
+                                                Pedido pedido = PedidoControlador.GetPedidoById(conexion, idPedido);
+
+                                                if (pedido != null)
+                                                {
+                                                    if (totalPaidAmount >= (pedido.precioTotal + pedido.precioEnvio))
+                                                    {
+                                                        pedido.idEstado = 1;
+                                                        pedido.PaidAmount = totalPaidAmount;
+                                                        PedidoControlador.updateEstadoPedido(conexion, pedido);
+
+                                                        string serverKeyFCM = WebConfigurationManager.AppSettings["serverKeyFCM"];
+                                                        string urlFCM = WebConfigurationManager.AppSettings["urlFCM"];
+
+                                                        NotificacionPushControlador.sendNotificationPushNuevoPedidoCliente(conexion, serverKeyFCM, urlFCM, pedido.id, pedido.idUsuario);
+                                                        _logger.Info("PaymentNotification: sendNotificationPushNuevoPedidoCliente: idUsuario = " + pedido.idUsuario);
+
+                                                        NotificacionPushControlador.sendNotificationPushNuevoPedidoRestaurante(conexion, serverKeyFCM, urlFCM, pedido.id, pedido.idRestaurante);
+                                                        _logger.Info("PaymentNotification: sendNotificationPushNuevoPedidoRestaurante: idRestaurante = " + pedido.idRestaurante);
+                                                    }
+                                                    else
+                                                    {
+                                                        _logger.Error("PaymentNotification: Se informó un pago de $ " + totalPaidAmount + " que es menor al valor total del pedido con id " + idPedido);
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    _logger.Error("PaymentNotification: No se encontró el pedido con id " + idPedido);
+                                                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                                                }
+                                            }
+                                            else
+                                            {
+                                                _logger.Error("PaymentNotification: Error al intentar obtener el monto del pago id " + paymentId);
+                                                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    _logger.Error("PaymentNotification: No se pudo obtener referencia externa para el pago id " + paymentId);
+                                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                                }
+                            }
+                            else
+                            {
+                                _logger.Error("PaymentNotification: Response Status: " + httpResponse.StatusCode);
+                                return new HttpResponseMessage(httpResponse.StatusCode);
+                            }
+                        }
+                        else
+                        {
+                            _logger.Error("PaymentNotification: No fue posible obtener los datos del pago");
+                            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error("PaymentNotification: " + e.Message);
+
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
